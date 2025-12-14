55caadd6055d1b23016e8e2b852fb24ef3449e0b
Bpull Mandal <notd2256@gmail.com>
2025-12-14 23:14:21 +0545

    Add files via upload

---
+++ a.php
@@ -0,0 +1,12 @@
+<?php
+
+// function hello($name){
+//     echo "hello $name";
+// }
+// require "function.php";
+// // hello("Ram");
+// require "xyz.php";
+// echo "hello after";
+
+include "b.php";
+include "c.php";
---
+++ c.php
@@ -0,0 +1,8 @@
+<?php
+
+function hello($name){
+    echo "hello $name";
+}
+hello("Ram");
+
+?>
